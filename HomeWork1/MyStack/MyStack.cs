51bf3d6f889df74e2ceb92582da8d39842dbbea1
AmenaynPadre <nsimonyan768@gmail.com>
2024-08-19 23:58:48 +0400

    asd

---
+++ HomeWork1/MyStack/MyStack.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyStack
+{
+    public class MyStack<T> : ICollection, IEnumerable<T>, IEnumerable
+    {
+        private T[] _items;
+        private int _count;
+        private int _defaultCapacity = 4;
+
+        public MyStack()
+        {
+            _items = new T[_defaultCapacity];
+        }
+        public MyStack(int capacity)
+        {
+            Capacity = capacity;
+            _items = new T[Capacity];
+        }
+        //public MyStack() 
+        //{ 
+
+        //}
+
+        public int Count { get => _count; }
+
+        public int Capacity { get; set; }
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        //public T Pop() 
+        //{
+
+        //}
+        public bool TryPop() { throw new NotImplementedException(); }
+
+        public void Push(T item)
+        {
+            if (_count == _items.Length)
+            {
+                Resize(_items.Length * 2);
+            }
+            _items[_count++] = item;
+        }
+
+        public bool TryPeek(out T result)
+        {
+            if (_count == 0)
+            {
+                result = default;
+                return false;
+            }
+            result = _items[_count - 1];
+            return true;
+        }
+
+        public T Peek()
+        {
+            if (_count == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+            return _items[_count - 1];
+        }
+
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_items, 0, _count);
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Resize(int newSize)
+        {
+            T[] newArray = new T[newSize];
+            Array.Copy(_items, newArray, _count);
+            Capacity = newSize;
+            _items = newArray;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ HomeWork1/MyStack/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var y = new MyStack<int>();
+
+            y.Push(1);
+            y.Push(2);
+            y.Push(3);
+            y.Push(4);
+            y.Push(5);
+
+            //Console.WriteLine(y.Peek());
+
+            Console.WriteLine(y.Count);
+            Console.WriteLine(y.Capacity);
+
+            //foreach (var item in y)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
